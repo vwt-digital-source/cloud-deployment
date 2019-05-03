dc93cbbcbadc9b17bea75ff4a2e44528dd93a6ae
Bernie van Veen <b.vanveen@vwt.digital>
2019-05-03 09:47:20 +0200

    Fixed cloudbuild. DAT-31

--- functions/publish-build-result-func/main.py
+++ functions/publish-build-result-func/main.py
@@ -1,5 +1,5 @@
 import base64
-
+from google.cloud import pubsub_v1
 
 def publish_build_result_pubsub(data, context):
     """Background Cloud Function to be triggered by Pub/Sub.
@@ -14,3 +14,11 @@ def publish_build_result_pubsub(data, context):
     else:
         name = 'World'
     print('Hello {}!'.format(name))
+    publisher = pubsub_v1.PublisherClient()
+    topic_name = 'projects/{project_id}/topics/{topic}'.format(
+        project_id=os.getenv('GOOGLE_CLOUD_PROJECT'),
+        topic='MY_TOPIC_NAME',  # Set this to something appropriate.
+    )
+    publisher.publish(topic_name, b'My first message!', spam='eggs')
+
+

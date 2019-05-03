d4d4242d761b06857d94f4de69f6133c0516bb40
Bernie van Veen <b.vanveen@vwt.digital>
2019-05-03 11:55:14 +0200

    Fixed use of env vars. DAT-31

--- functions/publish_build_result_func/main.py
+++ functions/publish_build_result_func/main.py
@@ -2,6 +2,7 @@ import base64
 import os
 from google.cloud import pubsub_v1
 
+
 def publish_build_result_func(data, context):
     """Background Cloud Function to be triggered by Pub/Sub.
     Args:
@@ -15,7 +16,7 @@ def publish_build_result_func(data, context):
         print('Hello {}!'.format(name))
         publisher = pubsub_v1.PublisherClient()
         topic_name = 'projects/{project_id}/topics/{topic}'.format(
-            project_id=os.getenv('PUBLISH_PROJECT_ID'),
-            topic='PUBLISH_TOPIC_NAME',
+            project_id=os.environ['PUBLISH_PROJECT_ID'],
+            topic=os.environ['PUBLISH_TOPIC_NAME'],
         )
         publisher.publish(topic_name, data['data'])

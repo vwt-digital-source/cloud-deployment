5a5b38f0d15c7f6d79ca499859b0b2d3898d4e6b
Bernie van Veen <b.vanveen@vwt.digital>
2019-05-03 11:16:44 +0200

    Specify topic to publish to. DAT-31

---
+++ functions/publish_build_result_func/main.py
@@ -0,0 +1,21 @@
+import base64
+import os
+from google.cloud import pubsub_v1
+
+def publish_build_result_func(data, context):
+    """Background Cloud Function to be triggered by Pub/Sub.
+    Args:
+         data (dict): The dictionary with data specific to this type of event.
+         context (google.cloud.functions.Context): The Cloud Functions event
+         metadata.
+    """
+
+    if 'data' in data:
+        name = base64.b64decode(data['data']).decode('utf-8')
+        print('Hello {}!'.format(name))
+        publisher = pubsub_v1.PublisherClient()
+        topic_name = 'projects/{project_id}/topics/{topic}'.format(
+            project_id=os.getenv('PUBLISH_PROJECT_ID'),
+            topic='PUBLISH_TOPIC_NAME',
+        )
+        publisher.publish(topic_name, data['data'])

06589b223b6fca4a27d2b06c181faeae48d0043c
dvanrhenen <d.vanrhenen@vwt.digital>
2019-09-27 11:45:36 +0200

    Added KMS to creating cloudbuilders group. DAT-938

--- scripts/create_cloudbuilds_group_iam.py
+++ scripts/create_cloudbuilds_group_iam.py
@@ -1,47 +1,43 @@
 import json
 import sys
 import os
+import base64
 
+from google.cloud import kms_v1
 from google.oauth2 import service_account
 import googleapiclient.discovery
 
-def get_service(api_name, api_version, scope, key_file_location, user_impersonation_email):
-    """Get a service that communicates to a Google API.
 
-    Args:
-        api_name: The name of the api to connect to.
-        api_version: The api version to connect to.
-        scopes: A list auth scopes to authorize for the application.
-        key_file_location: The path to a valid service account JSON key file.
+if len(sys.argv) >= 1:
+    projectsfile = open(sys.argv[1])
+    projects = json.load(projectsfile)
 
-    Returns:
-        A service that is connected to the specified API.
-    """
-
-    # Create credentials
-    credentials = service_account.Credentials.from_service_account_file(
-        key_file_location, scopes=[scope])
-
-    delegated_credentials = credentials.with_subject(user_impersonation_email)
-
-    service = googleapiclient.discovery.build(api_name, api_version, credentials=delegated_credentials)
-
-    return service
+    # Decrypt client_credentials to file
+    adminsdk_credentials_encrypted = base64.b64decode(os.environ['ADMINSDK_CREDENTIALS_ENCRYPTED'])
+    kms_client = kms_v1.KeyManagementServiceClient()
+    crypto_key_name = kms_client.crypto_key_path_path(os.environ['PROJECT_ID'], 'europe-west1', 'admin-sdk', 'admin-sdk-credentials')
+    decrypt_response = kms_client.decrypt(crypto_key_name, adminsdk_credentials_encrypted)
+    with open('adminsdk_credentials.json', 'w') as outfile:
+        json.dump(decrypt_response.plaintext, outfile)
 
 
-def set_group(service, group_key):
-    """Set a group within the G Suite domain.
+    # Set ADMIN SDK credentials
+    delegated_credentials = service_account.Credentials.from_service_account_file(
+        'adminsdk_credentials.json',
+        scopes=[os.environ['SERVICE_ACCOUNT_SCOPE']],
+        subject=os.environ['USER_IMPERSONATION_EMAIL'])
 
-    Args:
-        service: A service that is connected to the specified API.
-    """
+    service = googleapiclient.discovery.build(
+        'admin', 'directory_v1', credentials=delegated_credentials)
 
-    print(F'Group "{group_key}" is active')
-    # Getting groups by email
+    print('Service has been created')
+
+
+    # Set group
     group_object = service.groups().list(
         domain='vwt.digital',
         maxResults=1,
-        query='email:{}*'.format(group_key.split('@')[0])
+        query='email:{}*'.format(os.environ['GSUITE_GROUP_KEY']).split('@')[0])
     ).execute()
 
     # Create new group if not existing
@@ -49,21 +45,16 @@ def set_group(service, group_key):
         group_json = {
             "description": "All GCP cloud builder accounts",
             "name": "Cloud Builders",
-            "email": '{}'.format(group_key)
+            "email": '{}'.format(os.environ['GSUITE_GROUP_KEY']))
         }
         service.groups().insert(body=group_json).execute()
 
+    print(F'Group "{os.environ['GSUITE_GROUP_KEY'])}" is active')
 
-def set_members(service, group_key, projects_list):
-    """Set all members within a group based on a dictionary.
-
-    Args:
-        service: A service that is connected to the specified API.
-    """
 
     # Getting all members of the group
     members_object = service.members().list(
-        groupKey=group_key,
+        groupKey=os.environ['GSUITE_GROUP_KEY']),
         roles='MEMBER'
     ).execute()
     group_members = members_object.get('members')
@@ -72,47 +63,21 @@ def set_members(service, group_key, projects_list):
     if group_members:
         for member in group_members:
             service.members().delete(
-                groupKey=group_key,
+                groupKey=os.environ['GSUITE_GROUP_KEY']),
                 memberKey=member['id']
             ).execute()
 
         print('All current group members are removed')
 
-        # Add members to group
-        for project in projects_list:
-            new_user = {
-                "status": "ACTIVE",
-                "delivery_settings": "NONE",
-                "role": "MEMBER",
-                "type": "USER",
-                "email": '{}@appspot.gserviceaccount.com'.format(project['projectId'])
-            }
-            service.members().insert(groupKey=group_key, body=new_user).execute()
+    # Add members to group
+    for project in projects:
+        new_user = {
+            "status": "ACTIVE",
+            "delivery_settings": "NONE",
+            "role": "MEMBER",
+            "type": "USER",
+            "email": '{}@appspot.gserviceaccount.com'.format(project['projectId'])
+        }
+        service.members().insert(groupKey=os.environ['GSUITE_GROUP_KEY']), body=new_user).execute()
 
-        print('All new group members are added')
-
-
-def main():
-    if len(sys.argv) >= 1:
-        projectsfile = open(sys.argv[1])
-        projects = json.load(projectsfile)
-
-        service = get_service(
-            api_name='admin',
-            api_version='directory_v1',
-            scope=os.environ['SERVICE_ACCOUNT_SCOPE'],
-            key_file_location=os.environ['SERVICE_ACCOUNT_JSON_FILE_PATH'],
-            user_impersonation_email=os.environ['USER_IMPERSONATION_EMAIL']
-        )
-
-        set_group(
-            service=service,
-            group_key=os.environ['GSUITE_GROUP_KEY'])
-        set_members(
-            service=service,
-            group_key=os.environ['GSUITE_GROUP_KEY'],
-            projects_list=projects)
-
-
-if __name__ == '__main__':
-    main()
+    print('All new group members are added')

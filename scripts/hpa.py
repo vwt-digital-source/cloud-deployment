a2967ac819b78c4e7038680fcccd8e0abd8428c8
Eric van der Velde <e.vandervelde@vwt.digital>
2019-12-27 12:30:49 +0100

    Added HPA script. DAT-1467

--- scripts/hpa.py
+++ scripts/hpa.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import json
 from git import Repo
@@ -46,25 +47,33 @@ def set_policy(project_id, policy):
     return policy
 
 
+if len(sys.argv) < 2:
+    sys.exit()
+
+project_id = sys.argv[1]
+
+
 # Get the last commit
-repo = Repo('.')
-last_commit = list(repo.iter_commits(paths='config/{}'.format(os.environ['PROJECT_ID'])))[0]
+repo = Repo('')
+#repo = Repo('~/Documents/vwt-digital-config/high-privilege-access-config')
+last_commit = list(repo.iter_commits(paths='config/{}'.format(project_id)))[0]
 
 for request_file, v in last_commit.stats.files.items():
-    with open(request_file) as json_file:
-        hpa_request = json.load(json_file)
+    if request_file.find('config/{}'.format(project_id)) != -1:
+        with open(request_file) as json_file:
+            hpa_request = json.load(json_file)
 
-    if 'operational_access' in hpa_request:
-        oa = hpa_request['operational_access'][0]
+        if 'operational_access' in hpa_request:
+            oa = hpa_request['operational_access'][0]
 
-        if 'odrlPolicy' in oa:
-            policy = oa['odrlPolicy']
-            for permission in policy['permission']:
-                print(permission)
+            if 'odrlPolicy' in oa:
+                policy = oa['odrlPolicy']
+                for permission in policy['permission']:
+                    print(permission)
 
-                print("Read")
-                iam_policy = get_policy(permission['target'])
-                print("Change")
-                new_iam_policy = modify_policy_add_member(iam_policy, permission['action'], permission['assignee'])
-                print("Write")
-                set_policy(permission['target'], new_iam_policy)
+                    print("Read")
+                    iam_policy = get_policy(permission['target'])
+                    print("Change")
+                    new_iam_policy = modify_policy_add_member(iam_policy, permission['action'], permission['assignee'])
+                    print("Write")
+                    set_policy(permission['target'], new_iam_policy)

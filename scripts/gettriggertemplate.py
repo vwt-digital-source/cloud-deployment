0e34d0cf8b1290b3342fe117049bb6947a547863
Eric van der Velde <e.vandervelde@vwt.digital>
2020-01-02 16:21:01 +0100

    Fixed RepoSource object in running cloud build trigger. DAT-1575

--- scripts/gettriggertemplate.py
+++ scripts/gettriggertemplate.py
@@ -1,12 +1,23 @@
 import sys
 import json
 
+# RepoSource : https://cloud.google.com/cloud-build/docs/api/reference/rest/Shared.Types/Build#RepoSource
+# triggertTemplate:
+# { 'projectId': 'vwt-d-gew1-dat-deployment',
+#   'repoName': 'github_vwt-digital-config_dat-deployment-config',
+#   'branchName': 'develop'}
+#
+# github:
+# { 'projectId': 'vwt-d-gew1-dat-deployment',
+#   'repoName': 'high-privilege-access-config',
+#   'branchName': 'develop'}
 
 jsonfile = open(sys.argv[1], "r")
 triggers = json.loads(jsonfile.read())
 
-repoName = sys.argv[2]
-branchName = sys.argv[3]
+projectId = sys.argv[2]
+repoName = sys.argv[3]
+branchName = sys.argv[4]
 
 for trigger in triggers['triggers']:
     if 'triggerTemplate' in trigger:
@@ -14,4 +25,8 @@ for trigger in triggers['triggers']:
             print(trigger['triggerTemplate'])
     if 'github' in trigger:
         if trigger['github']['name'] == repoName and trigger['github']['push']['branch'] == branchName:
-            print(trigger['github'])
+            RepoSource = {}
+            RepoSource['projectId'] = projectId
+            RepoSource['repoName'] = trigger['github']['name']
+            RepoSource['branchName'] = trigger['github']['push']['branch']
+            print(RepoSource)

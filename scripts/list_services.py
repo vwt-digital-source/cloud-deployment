0b3bc6c0d7c78feec84ba41dcd7e0dc197d9f200
j.vanenckevort <j.vanenckevort@vwt.digital>
2020-03-29 11:00:05 +0200

    DAT-2166. Disable unspecified services

---
+++ scripts/list_services.py
@@ -0,0 +1,27 @@
+import sys
+import json
+
+project_id = sys.argv[1]
+
+default_services = [
+    'cloudtrace.googleapis.com',
+    'logging.googleapis.com',
+    'monitoring.googleapis.com',
+    'servicecontrol.googleapis.com',
+    'servicemanagement.googleapis.com',
+    'cloudresourcemanager.googleapis.com',
+    'source.googleapis.com',
+    'sourcerepo.googleapis.com',
+    'storage-api.googleapis.com',
+    'storage-component.googleapis.com',
+    'containerregistry.googleapis.com'
+]
+
+with open(sys.argv[2]) as projects_file:
+    projects = json.load(projects_file)
+    for project in projects.get('projects', []):
+        if project.get('projectId') == project_id:
+            services = project.get('services', [])
+            services.extend(default_services)
+            for service in services:
+                print(service)

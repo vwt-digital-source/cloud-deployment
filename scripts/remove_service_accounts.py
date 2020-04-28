55dbd1396e687cb7b8091c710ae955476b09d963
j.vanenckevort <j.vanenckevort@vwt.digital>
2020-04-28 16:16:40 +0200

    DAT-2710. Change update iam to include default service accounts

--- scripts/remove_service_accounts.py
+++ scripts/remove_service_accounts.py
@@ -1,46 +1,82 @@
 import json
-import sys
+import logging
+import argparse
+
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 
-credentials = GoogleCredentials.get_application_default()
-service = discovery.build('iam', 'v1', credentials=credentials)
 
-documented_serviceaccounts = []
-active_serviceaccounts = []
-to_be_removed_service_accounts = []
+def main(args):
+    service = make_service()
 
-if len(sys.argv) >= 1:
-    projectsfile = open(sys.argv[1])
-    projects = json.load(projectsfile)
+    projects = json.load(open(args.projects_file))
+    default_service_accounts = json.load(open(args.service_accounts_file)).get('default', [])
 
-    for pr in projects['projects']:
-        if 'serviceAccounts' in pr:
-            for sa in pr['serviceAccounts']:
-                documented_serviceaccounts.append({"projectId": pr['projectId'], "serviceAccount": sa})
+    documented_service_accounts = []
+    active_service_accounts = []
 
-        request = service.projects().serviceAccounts().list(name="projects/{}".format(pr['projectId']))
-        while True:
-            response = request.execute()
+    for project in projects.get('projects', []):
 
-            for service_account in response.get('accounts', []):
-                active_serviceaccounts.append(service_account)
+        for service_account in project.get('serviceAccounts', []):
+            documented_service_accounts.append({
+                "projectId": project['projectId'],
+                "serviceAccount": service_account
+            })
 
-            request = service.projects().serviceAccounts().list_next(previous_request=request, previous_response=response)
-            if request is None:
-                break
+        for service_account in default_service_accounts:
+            documented_service_accounts.append({
+                "projectId": project['projectId'],
+                "serviceAccount": service_account
+            })
 
-    for sa in active_serviceaccounts:
-        remove = False
-        if 'iam.gserviceaccount.com' in sa['name']:
-            if not next((item for item in documented_serviceaccounts if "projects/{}/serviceAccounts/{}".format(item['projectId'], item['serviceAccount']) in sa['name']), False):
-                remove = True
-            else:
-                print("FOUND {}".format(sa))
+        active_service_accounts.append(get_service_accounts(service, project['projectId']))
 
-        if remove:
-            to_be_removed_service_accounts.append(sa)
+    for service_account in active_service_accounts:
+        logging.info('FOUND {}'.format(service_account))
+        if 'iam.gserviceaccount.com' not in service_account['name']:
+            continue
+        for item in documented_service_accounts:
+            if not "projects/{}/serviceAccounts/{}".format(item['projectId'], item['serviceAccount']) in service_account['name']:
+                delete_service_account(service, service_account)
 
-    for sa in to_be_removed_service_accounts:
-        service.projects().serviceAccounts().delete(name=sa['name']).execute()
-        print("remove {}".format(sa['name']))
+
+def get_service_accounts(service, project_id):
+    service_accounts = []
+    request = service.projects().serviceAccounts().list(
+        name="projects/{}".format(project_id))
+    while True:
+        response = request.execute()
+        for service_account in response.get('accounts', []):
+            service_accounts.append(service_account)
+        request = service.projects().serviceAccounts().list_next(
+            previous_request=request,
+            previous_response=response)
+        if request is None:
+            break
+    return service_accounts
+
+
+def delete_service_account(service, service_account):
+    # service.projects().serviceAccounts().delete(name=service_account['name']).execute()
+    logging.info("REMOVED {}".format(service_account['name']))
+
+
+def make_service():
+    credentials = GoogleCredentials.get_application_default()
+    service = discovery.build('iam', 'v1', credentials=credentials)
+    return service
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Remove undocumented service accounts')
+    parser.add_argument('-p', '--projects-file',
+                        required=True,
+                        help='path to projects.json file')
+    parser.add_argument('-s', '--service-account-file',
+                        required=True,
+                        help='path to service_accounts.json file')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main(parse_args())

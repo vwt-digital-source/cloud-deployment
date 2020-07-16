3b1148d8e0ed1d6ab57f216376e60a96d4abade0
j.vanenckevort <j.vanenckevort@vwt.digital>
2020-07-16 10:51:29 +0200

    DAT-3777. Refactor runcloudbuildtrigger.sh

--- scripts/runcloudbuildtrigger.sh
+++ scripts/runcloudbuildtrigger.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-project_id=${1}
-repoName=${2}
-branchName=${3}
+PROJECT_ID=${1}
+REPO_NAME=${2}
+BRANCH_NAME=${3}
 
-if [ -z "${repoName}" ] || [ -z "${branchName}" ]
-then
-    echo "Please specify repository name and branch name to run the build trigger for"
-    echo "Syntax: $0 <projectId> <repoName> <branchName>"
-    exit 1
-fi
+function error_exit() {
+  # ${BASH_SOURCE[1]} is the file name of the caller.
+  echo "${BASH_SOURCE[1]}: line ${BASH_LINENO[0]}: ${1:-Unknown Error.} (exit ${2:-1})" 1>&2
+  exit "${2:-1}"
+}
 
-gcp_access_token=$(gcloud config config-helper --format='value(credential.access_token)')
-curl -X GET -H "Authorization: Bearer ${gcp_access_token}" "https://cloudbuild.googleapis.com/v1/projects/${project_id}/triggers" > triggers.json
-python3 gettriggertemplate.py triggers.json "${project_id}" "${repoName}" "${branchName}" | tee triggertemplate.json
-triggerid=$(python3 gettriggerid.py triggers.json "${repoName}" "${branchName}")
-echo "triggerid: ${triggerid}"
-curl -X POST -T triggertemplate.json -H "Authorization: Bearer ${gcp_access_token}" "https://cloudbuild.googleapis.com/v1/projects/${project_id}/triggers/${triggerid}:run"
+[[ -n "${PROJECT_ID}" ]] || error_exit "Missing required PROJECT_ID"
+[[ -n "${REPO_NAME}" ]] || error_exit "Missing required REPO_NAME"
+[[ -n "${BRANCH_NAME}" ]] || error_exit "Missing required BRANCH_NAME"
+
+
+trigger_id=$(gcloud beta builds triggers list \
+                --project="${PROJECT_ID}" \
+                --format="value(ID)" \
+                --filter="github.name=${REPO_NAME} \
+                          AND github.push.branch=${BRANCH_NAME} \
+                          AND filename=cloudbuild.yaml")
+
+gcloud beta builds triggers run "$trigger_id" \
+  --branch="${BRANCH_NAME}" \
+  --project="${PROJECT_ID}"

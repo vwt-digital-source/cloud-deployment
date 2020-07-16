6b524094f1027130089f787c41eb2383a23aee90
j.vanenckevort <j.vanenckevort@vwt.digital>
2020-07-16 15:14:26 +0200

    DAT-3721. Create project_cloudbuild.yaml

--- scripts/tmp_create_project.sh
+++ scripts/tmp_create_project.sh
@@ -2,10 +2,9 @@
 
 PROJECT_ID=${1}
 DEST_PROJECT=${2}
-PROJECT_CATALOG=${3}
-COMMON_CONFIG=${4}
-BILLING_ACCOUNT=${5}
-PARENT_ID=${6}
+COMMON_CONFIG=${3}
+BILLING_ACCOUNT=${4}
+PARENT_ID=${5}
 
 function error_exit() {
   # ${BASH_SOURCE[1]} is the file name of the caller.
@@ -15,16 +14,15 @@ function error_exit() {
 
 [[ -n "${PROJECT_ID}" ]] || error_exit "Missing required PROJECT_ID"
 [[ -n "${DEST_PROJECT}" ]] || error_exit "Missing required DEST_PROJECT"
-[[ -n "${PROJECT_CATALOG}" ]] || error_exit "Missing required PROJECT_CATALOG"
 [[ -n "${COMMON_CONFIG}" ]] || error_exit "Missing required COMMON_CONFIG"
 [[ -n "${BILLING_ACCOUNT}" ]] || error_exit "Missing required BILLING_ACCOUNT"
 [[ -n "${PARENT_ID}" ]] || error_exit "Missing required PARENT_ID"
 
+project_catalog="${COMMON_CONFIG}/projects/${DEST_PROJECT}/project.json"
+services="${COMMON_CONFIG}/services.json"
+iam_bindings="${COMMON_CONFIG}/iam_bindings.json"
+service_accounts="${COMMON_CONFIG}/service_accounts.json"
 deployment_name="${DEST_PROJECT}-project-deploy"
-project_catalog="${COMMON_CONFIG}/config/projects.json"
-services="${COMMON_CONFIG}/config/services.json"
-iam_bindings="${COMMON_CONFIG}/config/iam_bindings.json"
-service_accounts="${COMMON_CONFIG}/config/service_accounts.json"
 gcp_template=$(mktemp "${deployment_name}-XXXXX.py")
 
 {

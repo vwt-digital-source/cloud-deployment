5ddaf50e72a617518ccff495412ec07683be019a
SpringPie <bente.vandepol@gmail.com>
2021-08-11 11:18:15 +0200

    Addition for when filtering triggers does not work the regular way DAT-7273 ✓ changes have been validated

---
+++ projects/get_trigger_from_json.py
@@ -0,0 +1,37 @@
+import argparse
+import json
+
+
+def get_trigger(repo_name, branch_name, file_name, trigger_list_json):
+    triggers = trigger_list_json.get("triggers")
+    if triggers:
+        for trigger in triggers:
+            github = trigger.get("github")
+            if github:
+                github_name = github.get("name")
+                github_push = github.get("push")
+                if github_push:
+                    github_push_branch = github_push.get("branch")
+            filename = trigger.get("filename")
+            if (
+                filename == file_name
+                and github_name == repo_name
+                and github_push_branch == branch_name
+            ):
+                return trigger.get("id")
+    return None
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--repo-name", required=True)
+    parser.add_argument("-b", "--branch-name", required=True)
+    parser.add_argument("-f", "--file-name", required=True)
+    parser.add_argument("-t", "--trigger-list", required=True)
+    args = parser.parse_args()
+    repo_name = args.repo_name
+    branch_name = args.branch_name
+    file_name = args.file_name
+    trigger_list = args.trigger_list
+    trigger_list_json = json.loads(trigger_list)
+    print(get_trigger(repo_name, branch_name, file_name, trigger_list_json))
--- projects/run_cloudbuild_trigger.sh
+++ projects/run_cloudbuild_trigger.sh
@@ -3,6 +3,7 @@
 PROJECT_ID=${1}
 REPO_NAME=${2}
 BRANCH_NAME=${3}
+FILE_NAME='cloudbuild.yaml'
 
 function error_exit() {
   # ${BASH_SOURCE[1]} is the file name of the caller.
@@ -20,11 +21,22 @@ trigger_id=$(gcloud beta builds triggers list \
                 --format="value(ID)" \
                 --filter="github.name=${REPO_NAME} \
                           AND github.push.branch=${BRANCH_NAME} \
-                          AND filename=cloudbuild.yaml")
+                          AND filename=${FILE_NAME}")
 
 if [[ -n "${trigger_id}" ]]
 then
     gcloud beta builds triggers run "$trigger_id" \
       --branch="${BRANCH_NAME}" \
       --project="${PROJECT_ID}"
+else
+  trigger_ids=$(gcloud beta builds triggers list --format="json")
+  trigger_id_via_python=$(python3 ./get_trigger_from_json.py -r "${REPO_NAME}" -b "${BRANCH_NAME}" -t "${trigger_ids}" -f "${FILE_NAME}")
+  if [[ -z "${trigger_id_via_python}" ]]
+  then
+      echo "ERROR getting trigger."
+      exit 1
+  fi
+  gcloud beta builds triggers run "${trigger_id_via_python}" \
+    --branch="${BRANCH_NAME}" \
+    --project="${PROJECT_ID}"
 fi

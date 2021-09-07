ea36ef01cb89571f43645ddc1f7733b030f4c240
Rick van Sloten <r.vansloten@vwt.digital>
2021-09-07 11:37:56 +0200

    Conform to trigger list JSON DAT-7320 ✓ changes have been validated

--- projects/get_trigger_from_json.py
+++ projects/get_trigger_from_json.py
@@ -3,23 +3,21 @@ import json
 
 
 def get_trigger(repo_name, branch_name, file_name, trigger_list_json):
-    triggers = trigger_list_json.get("triggers")
-    if triggers:
-        for trigger in triggers:
-            github = trigger.get("github")
-            if github:
-                github_name = github.get("name")
-                github_push = github.get("push")
-                if github_push:
-                    github_push_branch = github_push.get("branch")
-            filename = trigger.get("filename")
-            if (
-                filename == file_name
-                and github_name == repo_name
-                and github_push_branch == branch_name
-            ):
-                return trigger.get("id")
-    return None
+    for trigger in trigger_list_json:
+        github = trigger.get("github")
+        if github:
+            github_name = github.get("name")
+            github_push = github.get("push")
+            if github_push:
+                github_push_branch = github_push.get("branch")
+        filename = trigger.get("filename")
+        if (
+            filename == file_name
+            and github
+            and github_name == repo_name
+            and github_push_branch == branch_name
+        ):
+            return trigger.get("id")
 
 
 if __name__ == "__main__":
